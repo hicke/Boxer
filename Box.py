260376228ecab9f422ed27d661b27b78862cd65c
Henric Helbro <henric.helbro@gmail.com>
2017-05-18 06:39:16 +0200

    First commit
    A first attempt to learn oo programming

---
+++ Box.py
@@ -0,0 +1,19 @@
+class SomeClass():
+
+    """
+    Using this space for a great comment
+    """
+
+    def __init__(self, variable):
+        self.variable = None
+
+    def afunction(self):
+        pass
+
+def main():
+    print('Hello World!')
+
+print(__name__)
+
+if __name__ == '__main__':
+    main()
