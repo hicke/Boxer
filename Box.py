9144342301039fe7fd59c4a7f36715b651150b6b
Henric Helbro <henric.helbro@gmail.com>
2017-05-19 22:29:49 +0200

    Working through som issues

--- Box.py
+++ Box.py
@@ -7,13 +7,14 @@ class SomeClass():
     def __init__(self, variable):
         self.variable = None
 
-    def afunction(self):
+    def afunction(self)
         pass
 
 def main():
     print('Hello World!')
 
-print(__name__)
 
-if __name__ == '__main__':
+
+print(__name__
+if __name__ == '__main__'
     main()
